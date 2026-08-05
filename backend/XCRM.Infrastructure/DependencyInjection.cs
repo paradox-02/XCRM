d34296e14f6e82c60f0128f8d612159b571d5b90
parado-02 <paradox_02@outlook.com>
2026-08-05 17:01:58 +0800

    Add user repository and dependency regiistration

---
+++ backend/XCRM.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+using XCRM.Domain.Repositories;
+using XCRM.Infrastructure.Data;
+using XCRM.Infrastructure.Repositories;
+
+namespace XCRM.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectinString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("未找到链接字符串 DefaultConnection");
+
+            services.AddDbContext<XCrmDbContext>(options =>
+            {
+                options.UseSqlServer(connectinString);
+            });
+
+            services.AddScoped<IsysUserRepository, SysUserRepository>();
+
+            return services;
+        }
+    }
+}
