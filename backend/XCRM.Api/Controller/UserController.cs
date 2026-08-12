7908e882665540907b4a9df6df27b5f7c753b126
parado-02 <paradox_02@outlook.com>
2026-08-12 08:52:34 +0800

    feat: add current user endpoint

--- backend/XCRM.Api/Controller/UserController.cs
+++ backend/XCRM.Api/Controller/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using XCRM.Application.Users;
 using XCRM.Application.Users.DTOs;
 
@@ -45,6 +46,26 @@ namespace XCRM.Api.Controller
                 nameof(GetById),
                 new { id = user.Id },
                 user);
+        }
+
+        [Authorize]
+        [HttpGet("me")]
+        public ActionResult GetCurrentUser()
+        {
+            var userIdValue = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var username = User.FindFirstValue(ClaimTypes.Name);
+
+            if (!long.TryParse(userIdValue, out var userId))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new
+            {
+                userId,
+                username
+            });
         }
     }
 }
